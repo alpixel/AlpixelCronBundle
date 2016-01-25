9df6c9f1b590567113f70149bc83abe22bdb5330
Benjamin HUBERT <benjamin@alpixel.fr>
2016-01-25 22:41:57 +0100

    debugging annotation autoloader

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,7 +3,7 @@
 $_SERVER['env'] = 'test';
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-if (!is_file($loaderFile = __DIR__.'/../vendor/autoload.php') && !is_file($loaderFile = __DIR__.'/../../../../../../vendor/autoload.php')) {
+if (!is_file($loaderFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
 
