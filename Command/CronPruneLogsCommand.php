90d7abe3110c5140a206163aeee26ecdb66ca049
WarGot <wargot@gmail.com>
2016-07-06 17:21:53 +0300

    fix style

--- Command/CronPruneLogsCommand.php
+++ Command/CronPruneLogsCommand.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Alpixel\Bundle\CronBundle\Entity\CronJob;
 
 class CronPruneLogsCommand extends ContainerAwareCommand
 {
@@ -29,16 +30,16 @@ class CronPruneLogsCommand extends ContainerAwareCommand
         }
 
         if ($job) {
-            $jobObj = $em->getRepository('CronBundle:CronJob')->findOneByCommand($job);
+            $jobObj = $em->getRepository(CronJob::class)->findOneByCommand($job);
             if (!$jobObj) {
                 $output->writeln("Couldn't find a job by the name of ".$job);
 
                 return CronJobResult::FAILED;
             }
 
-            $em->getRepository('CronBundle:CronJobResult')->deleteOldLogs($jobObj);
+            $em->getRepository(CronJobResult::class)->deleteOldLogs($jobObj);
         } else {
-            $em->getRepository('CronBundle:CronJobResult')->deleteOldLogs();
+            $em->getRepository(CronJobResult::class)->deleteOldLogs();
         }
 
         // Flush the EM
